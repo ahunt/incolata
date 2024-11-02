da7d1b350741f5e1641d54df1d88c3051ada0504
Andrzej Hunt <andrzej@ahunt.org>
2024-11-02 08:32:14 +0100

    Implement SequentialSeriesScrollingChartView

---
+++ sequentialseriesscrollingchartview.cpp
@@ -0,0 +1,176 @@
+#include "sequentialseriesscrollingchartview.h"
+
+#include <QLineSeries>
+#include <QLogValueAxis>
+#include <QValueAxis>
+
+SequentialSeriesScrollingChartView::SequentialSeriesScrollingChartView(
+  QWidget* const parent)
+  : QChartView(parent)
+  // QChartView already creates a chart for us during construction
+  // (https://github.com/qt/qtcharts/blob/4d0f13651edd605eacf9748ed46185a7d4d47e21/src/charts/qchartview.cpp#L284-L285),
+  // we only grab its pointer for convenience.
+  , mChart(this->chart())
+  , mXAxis(new QValueAxis())
+  , mSeriesList()
+  , mYAxisScalingMode(YAxisScalingMode::ZeroAnchored)
+  , mSeriesSpacing(0)
+  , mMinYSeen(std::numeric_limits<double>::infinity())
+  , mMaxYSeen(0.0)
+{
+  QValueAxis* const yAxis = new QValueAxis();
+  mYAxis = yAxis;
+
+  mXAxis->setLabelFormat("%d");
+  yAxis->setLabelFormat("%d");
+
+  mXAxis->setTickType(QValueAxis::TicksDynamic);
+  mXAxis->setTickAnchor(0);
+  mXAxis->setTickInterval(10);
+
+  mChart->legend()->hide();
+  mChart->addAxis(mXAxis, Qt::AlignBottom);
+  mChart->addAxis(mYAxis, Qt::AlignLeft);
+}
+
+QValueAxis*
+SequentialSeriesScrollingChartView::xAxis() const
+{
+  return mXAxis;
+}
+
+QAbstractAxis*
+SequentialSeriesScrollingChartView::yAxis() const
+{
+  return mYAxis;
+}
+
+void
+SequentialSeriesScrollingChartView::enableLogYAxis()
+{
+  assert(mSeriesList.size() == 0 &&
+         "enableLogYAxis() must only be called prior to adding datapoints");
+
+  mChart->removeAxis(mYAxis);
+  delete mYAxis;
+  QLogValueAxis* const axis = new QLogValueAxis();
+  mYAxis = axis;
+
+  mChart->addAxis(mYAxis, Qt::AlignLeft);
+
+  axis->setLabelFormat("%d");
+  axis->setBase(10.0);
+  axis->setMinorTickCount(-1);
+  axis->setMax(1000);
+}
+
+void
+SequentialSeriesScrollingChartView::setSeriesSpacing(const size_t& spacing)
+{
+  assert(mSeriesList.size() == 0 &&
+         "setSeriesSpacing() must only be called prior to adding datapoints");
+  mSeriesSpacing = spacing;
+}
+
+void
+SequentialSeriesScrollingChartView::setYAxisScalingMode(
+  const YAxisScalingMode& mode)
+{
+  assert(dynamic_cast<QValueAxis*>(mYAxis) &&
+         "setYAxisScalingMode must not be called for log Y axes");
+  mYAxisScalingMode = mode;
+}
+
+void
+SequentialSeriesScrollingChartView::setTitle(const QString& title)
+{
+  mChart->setTitle(title);
+}
+
+void
+SequentialSeriesScrollingChartView::setXRange(const size_t& range)
+{
+  mXRange = range;
+  refreshXRange();
+}
+
+void
+SequentialSeriesScrollingChartView::refreshXRange()
+{
+  size_t ceil = mXRange;
+  if (mSeriesList.size() > 0) {
+    ceil = std::max(
+      ceil, static_cast<size_t>(mSeriesList.back()->points().back().rx()));
+  }
+  mXAxis->setRange(ceil - mXRange, ceil);
+}
+
+void
+SequentialSeriesScrollingChartView::addDatapoint(const size_t& seriesIndex,
+                                                 const qreal& value)
+{
+  assert(seriesIndex <= mSeriesList.size() &&
+         "must not append to prior (already completed) series");
+
+  const QLineSeries* const lastPopulatedSeries =
+    mSeriesList.size() > 0 ? mSeriesList.back() : NULL;
+
+  QLineSeries* series;
+  while (seriesIndex >= mSeriesList.size()) {
+    // Add in a loop, because the caller could have skipped one or more
+    // intermediate series. We could make the vector sparse... (which is likely
+    // to be confusing to manage), or we can just add some empty series which
+    // makes things easier to reason about.
+    series = mSeriesList.emplace_back(new QLineSeries());
+
+    // Ordering is significant: Qt complains (and ignores the request) if
+    // you try to attach an axis to a new series, if that axis is already
+    // attached to another series which is attached to a chart, and if that
+    // new series is not yet attached to the chart. (Translation:
+    // addSeries(foo), before you foo->attachAxis.)
+    mChart->addSeries(series);
+    series->attachAxis(mXAxis);
+    series->attachAxis(mYAxis);
+
+    series->setPointsVisible(true);
+  }
+  series = mSeriesList.back();
+
+  qsizetype x;
+
+  if (series->points().length() > 0) {
+    x = series->points().back().rx() + 1;
+  } else {
+    if (lastPopulatedSeries) {
+      x = lastPopulatedSeries->points().back().rx() + mSeriesSpacing;
+    } else {
+      // First point, ever.
+      x = 0;
+    }
+  }
+
+  series->append(QPoint(x, value));
+  refreshXRange();
+
+  // Using something functional to find the min/max Y values would certainly be
+  // more elegant, but this is faster (at the cost of 16 bytes).
+  mMinYSeen = std::min(mMinYSeen, value);
+  mMaxYSeen = std::max(mMaxYSeen, value);
+  switch (mYAxisScalingMode) {
+    case ZeroAnchored: {
+      // TODO: configure making this configurable
+      const qsizetype max = static_cast<qsizetype>(mMaxYSeen) / 20 * 20 + 20;
+      mYAxis->setRange(0.0, static_cast<qreal>(max));
+      break;
+    }
+    case Floating: {
+      // TODO: consider making margins configurable.
+      const qsizetype min =
+        std::max(static_cast<qsizetype>(0),
+                 static_cast<qsizetype>(mMinYSeen) / 100 * 100 - 100);
+      const qsizetype max = static_cast<qsizetype>(mMaxYSeen) / 100 * 100 + 200;
+      mYAxis->setRange(static_cast<qreal>(min), static_cast<qreal>(max));
+      break;
+    }
+  }
+}
---
+++ sequentialseriesscrollingchartview.h
@@ -0,0 +1,78 @@
+#ifndef SEQUENTIALSERIESSCROLLINGCHARTVIEW_H
+#define SEQUENTIALSERIESSCROLLINGCHARTVIEW_H
+
+#include <QChartView>
+
+class QAbstractAxis;
+class QValueAxis;
+class QLineSeries;
+
+enum YAxisScalingMode
+{
+  ZeroAnchored,
+  Floating,
+};
+
+// SequentialSeriesScrollingChartView allows you to easily visualise render
+// sequential dataseries. It's raison d'être is the live sample/ambient/FF
+// graphs, where each series is discrete, but should follow directly from the
+// previous series. It can also be used as a single-series scrolling chart,
+// by only ever to appending to the series with index 0.
+class SequentialSeriesScrollingChartView : public QChartView
+{
+public:
+  SequentialSeriesScrollingChartView(QWidget* const parent = nullptr);
+
+  // The range of data to show on the X-Axis. The actual range is calculated
+  // dynamically as [max(x) - xRange, max(x)].
+  Q_PROPERTY(qsizetype xRange MEMBER mXRange WRITE setXRange);
+  void setXRange(const size_t& range);
+
+  Q_PROPERTY(QValueAxis* xAxis MEMBER mXAxis READ default);
+  QValueAxis* xAxis() const;
+
+  Q_PROPERTY(QAbstractAxis* yAxis MEMBER mYAxis READ default);
+  QAbstractAxis* yAxis() const;
+
+  // Spacing between each sequential series. All changes must be made prior to
+  // adding any datapoints. Defaults to 0, i.e. by default the first point of
+  // each series overlaps the last point of the previous series.
+  Q_PROPERTY(size_t seriesSpacing MEMBER default WRITE setSeriesSpacing);
+  void setSeriesSpacing(const size_t& spacing);
+
+  // Set the Chart's title.
+  void setTitle(const QString& title);
+  // Enables a log-scale Y Axis. Must be called prior to adding any datapoints;
+  // this action is irreversable.
+  void enableLogYAxis();
+  // Set the Y-Axis scaling mode. Must not be used if enableLogYAxis() is in
+  // use (it's 1-anchored anyway).
+  void setYAxisScalingMode(const YAxisScalingMode& mode);
+
+  // It is not permitted to add a datapoint to a previous series.
+  void addDatapoint(const size_t& seriesIndex, const qreal& value);
+
+private:
+  // Owned by QChartView (will be deleted by QChartView *unless* replaced via
+  // QChartView::setChart()).
+  QChart* const mChart;
+
+  // Owned by mChart.
+  QValueAxis* mXAxis;
+  // Owned by mChart.
+  QAbstractAxis* mYAxis;
+
+  // Each series is owned by mChart.
+  std::vector<QLineSeries*> mSeriesList;
+
+  YAxisScalingMode mYAxisScalingMode;
+  size_t mSeriesSpacing;
+  size_t mXRange;
+
+  qreal mMinYSeen;
+  qreal mMaxYSeen;
+
+  void refreshXRange();
+};
+
+#endif // SEQUENTIALSERIESSCROLLINGCHARTVIEW_H

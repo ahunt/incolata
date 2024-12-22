a517dea26d6a8692440275eb774479329739a136
Andrzej Hunt <andrzej@ahunt.org>
2024-12-22 14:10:46 +0100

    connectiondialog: build basic layout

--- connectiondialog.cpp
+++ connectiondialog.cpp
@@ -1,11 +1,17 @@
 #include "connectiondialog.h"
 #include "ui_connectiondialog.h"
 
+#include <QDialogButtonBox>
+#include <QPushButton>
+
 ConnectionDialog::ConnectionDialog(QWidget *parent) :
   QDialog(parent),
   ui(new Ui::ConnectionDialog)
 {
   ui->setupUi(this);
+
+  ui->buttonBox->button(QDialogButtonBox::Ok)->setText("Connect");
+  ui->warningIcon->setPixmap(QIcon::fromTheme("dialog-warning").pixmap(QSize(32, 32)));
 }
 
 ConnectionDialog::~ConnectionDialog()

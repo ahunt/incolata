ahunt/incolata

commit df37c08439a62e670db7b1e0d3bf3cdf040e43a2

fitfactorchartview: prepare for dual-device support
In preparation for:
https://github.com/ahunt/incolata/issues/4

fitfactorchartview.cpp
@@ -8,7 +8,7 @@ FitFactorChartView::FitFactorChartView(QWidget* const parent)
   : QChartView(parent)
   , mExerciseCount(8)
   , mMaxFFSeen(0)
-  , mSeries(new QLineSeries())
+  , mSeriess(2)
   , mXAxis(new QValueAxis())
   , mYAxis(new QLogValueAxis())
 {
@@ -24,13 +24,16 @@ FitFactorChartView::FitFactorChartView(QWidget* const parent)
   mYAxis->setBase(10.0);
   mYAxis->setMinorTickCount(-1);
 
-  chart()->addSeries(mSeries);
+  chart()->addSeries(mSeriess[0]);
+  chart()->addSeries(mSeriess[1]);
   chart()->addAxis(mXAxis, Qt::AlignBottom);
   chart()->addAxis(mYAxis, Qt::AlignLeft);
 
-  mSeries->setPointsVisible(true);
-  mSeries->attachAxis(mXAxis);
-  mSeries->attachAxis(mYAxis);
+  for (size_t i = 0; i < 2; i++) {
+    mSeriess[i]->setPointsVisible(true);
+    mSeriess[i]->attachAxis(mXAxis);
+    mSeriess[i]->attachAxis(mYAxis);
+  }
 
   chart()->setMargins(QMargins(5,5,5,5));
 
@@ -48,7 +51,8 @@ FitFactorChartView::setExerciseCount(const qsizetype& count)
 void
 FitFactorChartView::wipeData()
 {
-  mSeries->removePoints(0, mSeries->count());
+  mSeriess[0]->removePoints(0, mSeriess[0]->count());
+  mSeriess[1]->removePoints(0, mSeriess[1]->count());
   mMaxFFSeen = 0;
   // Do not reset y-axis max: there's no point, and there's a non-zero
   // probability that this test will have the same max as the last test (e.g. if
@@ -81,14 +85,15 @@ FitFactorChartView::refreshBackground()
 }
 
 void
-FitFactorChartView::addDatapoint(const size_t& exercise,
+FitFactorChartView::addDatapoint(const size_t& deviceIndex,
+                                 const size_t& exercise,
                                  const double& fitFactor)
 {
   assert(exercise <
            static_cast<size_t>(std::numeric_limits<qsizetype>::max()) + 1 &&
          "sorry, FTL doesn't support that many exercises");
 
-  mSeries->append(QPoint(exercise + 1, fitFactor));
+  mSeriess[deviceIndex]->append(QPoint(exercise + 1, fitFactor));
 
   // Round up to the next whole log as that seems easiest to read.
   mMaxFFSeen = std::max(mMaxFFSeen, fitFactor);

fitfactorchartview.h
@@ -21,14 +21,16 @@ public:
   void wipeData();
 
 public slots:
-  void addDatapoint(const size_t& exercise, const double& ff);
+  void addDatapoint(const size_t& deviceIndex,
+                    const size_t& exercise,
+                    const double& ff);
 
 private:
   qsizetype mExerciseCount;
   qreal mMaxFFSeen;
 
   // Owned by QChartView.chart().
-  QLineSeries* const mSeries;
+  std::vector<QLineSeries*> const mSeriess;
   // Owned by QChartView.chart().
   QValueAxis* const mXAxis;
   // Owned by QChartView.chart().

exercisesmodel.cpp
@@ -70,10 +70,14 @@ ExercisesModel::updateCurrentExercise(const uint aExercise)
 }
 
 void
-ExercisesModel::updateFF(const uint aExercise,
-                         const double aFitFactor,
-                         const double aErr)
+ExercisesModel::updateFF(const size_t& deviceIndex,
+			 const size_t& aExercise,
+                         const double& aFitFactor,
+                         const double& aErr)
 {
+  // TODO: handle deviceIndex.
+  (void) deviceIndex;
+
   mFitFactors[aExercise] = aFitFactor;
   mErrs[aExercise] = aErr;
   emit dataChanged(index(aExercise, 0),

exercisesmodel.h
@@ -18,7 +18,7 @@ public:
 
 public slots:
   void updateCurrentExercise(const uint aExercise);
-  void updateFF(const uint aExercise, const double aFitFactor, double aErr);
+  void updateFF(const size_t& aDeviceIndex, const size_t& aExercise, const double& aFitFactor, const double& aErr);
   void renderInterimFF(uint aExercise, double aFitFactor);
 
 private:

mainwindow.cpp
@@ -67,7 +67,8 @@ MainWindow::test_callback(const TestNotification* notification, void* cb_data)
       const size_t ex = notification->exercise_result._0;
       const double ff = notification->exercise_result._1;
       const double err = notification->exercise_result._2;
-      emit mw->ffUpdated(ex, ff, err);
+      // TODO: set correct device index
+      emit mw->ffUpdated(0, ex, ff, err);
       break;
     };
     case TestNotification::Tag::Sample: {

mainwindow.h
@@ -65,7 +65,7 @@ signals:
                    const QString& aSubject,
                    const QString& aComment);
   void exerciseChanged(uint ex);
-  void ffUpdated(const uint& exercise, const double& fitFactor, const double& err);
+  void ffUpdated(const size_t& aDeviceIndex, const size_t& aExercise, const double& aFitFactor, const double& aErr);
   void renderRawSample(QString sample);
   void receivedRawSample(double sample);
   void receivedSample(SampleType sampleType, size_t exercise, double value);

commit f6ec538ab24a70c3216c4ca447a29bf40859c7e7

fitfactorchartview: don't crash during construction
Introduced by: df37c08439a62e670db7b1e0d3bf3cdf040e43a2
As part of: https://github.com/ahunt/incolata/issues/4

fitfactorchartview.cpp
@@ -8,7 +8,7 @@ FitFactorChartView::FitFactorChartView(QWidget* const parent)
   : QChartView(parent)
   , mExerciseCount(8)
   , mMaxFFSeen(0)
-  , mSeriess(2)
+  , mSeriess({new QLineSeries(), new QLineSeries()})
   , mXAxis(new QValueAxis())
   , mYAxis(new QLogValueAxis())
 {

fitfactorchartview.h
@@ -29,7 +29,7 @@ private:
   qsizetype mExerciseCount;
   qreal mMaxFFSeen;
 
-  // Owned by QChartView.chart().
+  // Members are owned by QChartView.chart().
   std::vector<QLineSeries*> const mSeriess;
   // Owned by QChartView.chart().
   QValueAxis* const mXAxis;